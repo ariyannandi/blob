fb3652f336d727f7d46f2d5552748ebc7fd9d6d3
ariyannandi <ariyannandi2711@gmail.com>
2023-02-23 23:11:32 +0530

    added some more visual changes and features to PostCard component.

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,9 +12,7 @@ const Home = ({ posts }) => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <div>
-              <PostCard post={post} key={post.title} />
-            </div>
+            <PostCard post={post.node} key={index} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
